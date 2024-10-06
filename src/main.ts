4333be60d2cea8bb9185dd313953dc901c60f42e
carprulez <147204410+carprulez@users.noreply.github.com>
2024-10-06 10:05:33 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,12 @@ header.innerHTML = gameName;
 button.innerHTML = buttonName;
 app.append(header);
 app.append(button);
+
+const counterDiv = document.getElementById("counter") as HTMLDivElement;
+
+let counter: number = 0;
+
+button.addEventListener("click", () => {
+    counter++;
+    counterDiv.innerHTML = `Grafts made: ${counter}`;
+});
