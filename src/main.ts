1030c9c2261a77171f5ad5dd27e3a54c68eab5e7
carprulez <147204410+carprulez@users.noreply.github.com>
2024-10-05 22:27:15 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Arborist's Adventure";
+const gameName = "Leaf Lovers";
+const buttonName = "🌳";
 document.title = gameName;
 
 const header = document.createElement("h1");
+const button = document.createElement("b1");
 header.innerHTML = gameName;
+button.innerHTML = buttonName;
 app.append(header);
+app.append(button);
+
+
