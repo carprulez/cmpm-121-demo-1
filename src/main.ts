bad22d32bc0d09dd82e5380cb53c99995d757403
carprulez <147204410+carprulez@users.noreply.github.com>
2024-10-06 10:28:36 -0700

    Step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const buttonName = "🌳";
 document.title = gameName;
 
 const header = document.createElement("h1");
-const button = document.createElement("b1");
+const button = document.createElement("button");
 header.innerHTML = gameName;
 button.innerHTML = buttonName;
 app.append(header);
@@ -17,7 +17,17 @@ const counterDiv = document.getElementById("counter") as HTMLDivElement;
 
 let counter: number = 0;
 
+// Increment counter every time the user clicks the button
 button.addEventListener("click", () => {
-  counter++;
-  counterDiv.innerHTML = `Grafts made: ${counter}`;
-});
+    incrementCounter();
+  });
+  
+  // Increment counter every second using setInterval
+  setInterval(() => {
+    incrementCounter();
+  }, 1000);
+  
+  function incrementCounter() {
+    counter++;
+    counterDiv.innerHTML = `Grafts made: ${counter}`;
+  }
